942cb086b8a386e056110119b777304fc17fb1de
Hugo Peixoto <hugo.peixoto@gmail.com>
2011-11-04 00:42:11 +0000

    Adds a simple "test" file

---
+++ test.cc
@@ -0,0 +1,47 @@
+
+#include "light_sax.h"
+
+class printer_handler : public light_sax::handler_t
+{
+  public:
+  printer_handler ();
+  virtual void start_element (char* a_name, char* a_attributes[][2]);
+  virtual void end_element (char* a_name);
+  virtual void characters (char* ch, int start, int length);
+
+  protected:
+  unsigned int depth_;
+};
+
+printer_handler::printer_handler ()
+{
+  depth_ = 0;
+}
+
+void printer_handler::start_element (char* a_name, char* a_attributes[][2])
+{
+  printf("%*sOpening tag: %s\n", 2*depth_, "", a_name);
+  ++depth_;
+}
+
+void printer_handler::end_element (char* a_name)
+{
+  --depth_;
+  printf("%*sEnding tag: %s\n", depth_, "  ", a_name);
+}
+
+void printer_handler::characters (char* a_characters, int start, int length)
+{
+}
+
+/**
+ * @brief Reads an XML document from stdin 
+ */
+int main ()
+{
+  printer_handler handler;
+  light_sax::parser_t parser(stdin, &handler);
+  parser.parse();
+  return 0;
+}
+
